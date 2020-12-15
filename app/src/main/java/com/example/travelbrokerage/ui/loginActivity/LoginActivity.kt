b2cbe8efafaee0db2319bb5a9ae25eb264ad59e1
yehuda kahan <yehudajka@gmail.com>
2020-12-15 17:24:19 +0200

    LoginActivity.kt SharedPreference progressBar

--- app/src/main/java/com/example/travelbrokerage/ui/loginActivity/LoginActivity.kt
+++ app/src/main/java/com/example/travelbrokerage/ui/loginActivity/LoginActivity.kt
@@ -1,10 +1,15 @@
 package com.example.travelbrokerage.ui.loginActivity
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Patterns
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -27,8 +32,10 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var btnCreateAccount: MaterialButton
     private lateinit var awesomeValidation: AwesomeValidation
     private lateinit var warningMassage: TextView
+    private lateinit var progressBar: ProgressBar
 
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,20 +44,30 @@ class LoginActivity : AppCompatActivity() {
         etMail = findViewById(R.id.et_mail)
         etPassword = findViewById(R.id.et_password)
         warningMassage = findViewById(R.id.tv_massage)
+        progressBar = findViewById(R.id.progressBar)
 
         mAuth = FirebaseAuth.getInstance()
         setValidation()
+        sharedPreferences = getSharedPreferences("USER", MODE_PRIVATE)
+        updateFields()
     }
 
+    @SuppressLint("CommitPrefEdits")
     fun loginOnclick(view: View) {
         if (awesomeValidation.validate()) {
             warningMassage.text = ""
+            progressBar.visibility = VISIBLE
             val email = etMail.text.toString()
             val password = etPassword.text.toString()
+
             mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-
+                        progressBar.visibility = INVISIBLE
+                        val editor = sharedPreferences.edit()
+                        editor.putString("Mail", email)
+                        editor.putString("Password", password)
+                        editor.apply()
                         val user = mAuth.currentUser
                         if (user?.isEmailVerified == true) {
                             val i = Intent(this, MainActivity::class.java)
@@ -59,6 +76,7 @@ class LoginActivity : AppCompatActivity() {
                             warningMassage.text = "אנא אמת את החשבון על ידי המייל שנשלח אליך"
                         }
                     } else {
+                        progressBar.visibility = INVISIBLE
                         warningMassage.text = "כתובת מייל או סיסמה אינם נכונים"
                     }
                 }
@@ -68,6 +86,7 @@ class LoginActivity : AppCompatActivity() {
     fun createAccountOnClick(view: View) {
         if (awesomeValidation.validate()) {
             warningMassage.text = ""
+            progressBar.visibility = VISIBLE
             val email = etMail.text.toString()
             val password = etPassword.text.toString()
 
@@ -78,12 +97,14 @@ class LoginActivity : AppCompatActivity() {
                         user?.sendEmailVerification()
                             ?.addOnCompleteListener(this) { tsk ->
                                 if (tsk.isSuccessful) {
+                                    progressBar.visibility = INVISIBLE
                                     Toast.makeText(
                                         applicationContext,
                                         "אנא אמת את החשבון על ידי המייל שנשלח אליך",
                                         Toast.LENGTH_LONG
                                     ).show()
                                 } else {
+                                    progressBar.visibility = INVISIBLE
                                     Toast.makeText(
                                         applicationContext,
                                         "שליחת האימות נכשלה. אנא נסה שנית מאוחר יותר",
@@ -92,6 +113,7 @@ class LoginActivity : AppCompatActivity() {
                                 }
                             }
                     } else {
+                        progressBar.visibility = INVISIBLE
                         warningMassage.text = "החשבון כבר קיים במערכת"
                     }
                 }
@@ -112,5 +134,10 @@ class LoginActivity : AppCompatActivity() {
             this, R.id.et_password,
             "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$", R.string.invalid_password
         )
+    }
+
+    private fun updateFields() {
+        etMail.setText(sharedPreferences.getString("Mail", ""))
+        etPassword.setText(sharedPreferences.getString("Password", ""))
     }
 }
