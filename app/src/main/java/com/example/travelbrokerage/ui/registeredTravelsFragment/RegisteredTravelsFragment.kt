e3e7ecfbfb57ba4a4711c3d4eeabaac345b562be
yehuda kahan <yehudajka@gmail.com>
2021-01-01 11:37:36 +0200

    first fragment continue with fix the bugs (in the way to make it work)

--- app/src/main/java/com/example/travelbrokerage/ui/registeredTravelsFragment/RegisteredTravelsFragment.kt
+++ app/src/main/java/com/example/travelbrokerage/ui/registeredTravelsFragment/RegisteredTravelsFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.travelbrokerage.R
 import com.example.travelbrokerage.adapters.AdapterCostumer
 import com.example.travelbrokerage.ui.homePage.MainActivityViewModel
+import com.example.travelbrokerage.util.MyApplication
 import java.util.*
+import kotlin.collections.ArrayList
 
 class RegisteredTravelsFragment : Fragment() {
 
@@ -20,15 +23,22 @@ class RegisteredTravelsFragment : Fragment() {
     }
 
     private lateinit var viewModel : MainActivityViewModel
+     lateinit var listView : ListView
+    lateinit var tmp : ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.registered_travels_fragment, container, false)
     }
 
+    override fun onStart() {
+        super.onStart()
+        loadData()
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val listView = view?.findViewById<ListView>(R.id.list_view_costumer)
+        listView = view?.findViewById<ListView>(R.id.list_view_costumer)!!
 
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
@@ -41,5 +51,9 @@ class RegisteredTravelsFragment : Fragment() {
             //set custom adapter as adapter to our list view
             listView?.adapter = adapter
         })
+    }
+
+    private fun loadData(){
+        viewModel.getTravels()
     }
 }
