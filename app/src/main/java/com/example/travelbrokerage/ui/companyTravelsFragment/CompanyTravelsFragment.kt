91112d51598543859e51141cc2fe15b84758a98e
zeev abramovich <57068932+zeviAbramovich@users.noreply.github.com>
2021-01-08 10:57:07 +0200

    i did the company row, and xml with list view, and the class of company travel fragment, need to do change in the adapter

--- app/src/main/java/com/example/travelbrokerage/ui/companyTravelsFragment/CompanyTravelsFragment.kt
+++ app/src/main/java/com/example/travelbrokerage/ui/companyTravelsFragment/CompanyTravelsFragment.kt
@@ -6,19 +6,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import androidx.lifecycle.Observer
 import com.example.travelbrokerage.R
+import com.example.travelbrokerage.adapters.AdapterCostumer
 import com.example.travelbrokerage.ui.homePage.MainActivityViewModel
 
 class CompanyTravelsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = CompanyTravelsFragment()
-    }
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var listView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.company_travels_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        listView = view?.findViewById<ListView>(R.id.list_view_company)!!
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        viewModel.getCostumerTravels().observe(viewLifecycleOwner, Observer { travels ->
+            val tmp = ArrayList(travels)
+
+            //create adapter object
+            val adapter = AdapterCostumer(requireContext(),viewModel, tmp)
+
+            //set custom adapter as adapter to our list view
+            listView.adapter = adapter
+        })
+        //viewModel.loadCostumerList()
     }
 }
